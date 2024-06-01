d7ea5e2c1612386e17f6f4e841aba9ec3ddcaf1e
Wiktor Krawczak <wiko3034gmail.com>
2024-06-01 11:14:33 +0200

    zad2

--- Main.java
+++ Main.java
@@ -1,17 +1,19 @@
+import java.io.File;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
-import java.io.File;
-
 public class Main extends Application {
     private TextField directoryPathField;
     private TextField searchField;
+    private TextArea resultArea;
 
     @Override
     public void start(Stage primaryStage) {
@@ -23,15 +25,18 @@ public class Main extends Application {
         searchField = new TextField();
         searchField.setPromptText("Enter search phrase");
 
+        resultArea = new TextArea();
+        resultArea.setPrefHeight(400);
+
         Button browseButton = new Button("Browse");
         browseButton.setOnAction(e -> browseDirectory());
 
         Button searchButton = new Button("Search");
 
         HBox hBox = new HBox(10, directoryPathField, browseButton);
-        VBox vBox = new VBox(10, hBox, searchField, searchButton);
+        VBox vBox = new VBox(10, hBox, searchField, searchButton, resultArea);
 
-        Scene scene = new Scene(vBox, 600, 200);
+        Scene scene = new Scene(vBox, 600, 400);
 
         primaryStage.setScene(scene);
         primaryStage.show();
